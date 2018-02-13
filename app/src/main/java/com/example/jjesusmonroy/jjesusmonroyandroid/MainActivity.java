dcca7ce8aa048f0d11eb243b806837338f842dbc
jjesusmonroy <ppchuyjjms@gmail.com>
2018-02-13 09:35:30 -0700

    Starting new project

--- app/src/main/java/com/example/jjesusmonroy/jjesusmonroyandroid/MainActivity.java
+++ app/src/main/java/com/example/jjesusmonroy/jjesusmonroyandroid/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.jjesusmonroy.jjesusmonroyandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lv = findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Addind just one");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+
     }
 }
